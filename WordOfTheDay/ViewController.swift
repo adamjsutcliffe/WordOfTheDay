5785759dd59e5763925278a2434ff31193b5baf1
adamjsutcliffe <adam@peak.net>
2019-11-14 17:13:08 +0000

    [ADD] modal pop over structure

--- WordOfTheDay/ViewController.swift
+++ WordOfTheDay/ViewController.swift
@@ -12,7 +12,6 @@ class ViewController: UIViewController {
 
     
     @IBOutlet weak var WordOfTheDayBgView: UIView!
-    @IBOutlet weak var WordOfTheDayLabel: UILabel!
     @IBOutlet weak var WordOfTheDayButton: UIButton!
     
     override func viewDidLoad() {
@@ -20,13 +19,14 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         WordOfTheDayBgView.layer.cornerRadius = 10
         
-        WordOfTheDayButton.layer.borderWidth = 1
-        WordOfTheDayButton.layer.borderColor = UIColor.purple.cgColor
+//        WordOfTheDayButton.layer.borderWidth = 1
+//        WordOfTheDayButton.layer.borderColor = UIColor.purple.cgColor
         WordOfTheDayButton.layer.cornerRadius = 5
         
-        WordOfTheDayLabel.text = "What is todays word of the day?\n\nCan you use it in the correct context?"
+//        WordOfTheDayLabel.text = "What is todays word of the day?\n\nCan you use it in the correct context?"
     }
 
-
+    @IBAction func cancelToMainViewController(_ segue: UIStoryboardSegue) {
+    }
 }
 
---
+++ WordOfTheDay/WordViewController.swift
@@ -0,0 +1,21 @@
+//
+//  WordViewController.swift
+//  WordOfTheDay
+//
+//  Created by Adam Sutcliffe on 14/11/2019.
+//  Copyright © 2019 Peak. All rights reserved.
+//
+
+import UIKit
+
+class WordViewController: UIViewController {
+    
+    @IBOutlet weak var wordLabel: UILabel!
+    @IBOutlet weak var definitionLabel: UILabel!
+    @IBOutlet weak var examplesLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
